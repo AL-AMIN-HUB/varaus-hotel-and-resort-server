11aa003722e056ce040e6b9d116b2f876aa97332
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-29 19:14:24 +0600

    added ordered

--- index.js
+++ index.js
@@ -29,22 +29,16 @@ async function run() {
       res.send(result);
     });
 
-    //
-    app.get("/orderPlace", async (req, res) => {
-      const result = await ordersCollection.find({}).toArray();
-      res.send(result);
-    });
-
     // Order Place
 
-    // app.get("/orderPlace/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const service = await servicesCollection.find(query);
+    app.get("/orderPlace/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
 
-    //   // console.log("load user with id", id);
-    //   res.send(service);
-    // });
+      // console.log("load user with id", id);
+      res.send(service);
+    });
 
     // order
     app.post("/orderPlace", async (req, res) => {
