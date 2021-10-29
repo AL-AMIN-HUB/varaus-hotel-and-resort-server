f08babb0dbbd4a42aaff87e7f828cf5e54ac8695
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-29 12:43:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require("mongodb");
+//
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware use
+app.use(cors());
+app.use(express.json());
+
+// connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4xkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("hotelResort");
+    const servicesCollection = database.collection("services");
+
+    // get service from database
+    app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find({}).toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//
+app.get("/", (req, res) => {
+  res.send("Happy Assignment Hotel and Resort");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
